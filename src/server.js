ac15a129db7940f3557506c116eb74e8d48e9713
Thomas Sanceo <tsanceo@yahoo.com>
2020-02-11 19:16:44 +0100

    heroku ok

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ import app from './app';
 import displayRoutes from 'express-routemap';
 
 app.listen(app.get('port'), () => {
-    console.log(
-        `App is running at http://localhost:${app.get('port')} in ${app.get('env')}`
-    );
-    console.log('Press CTRL-C to stop ');
-    displayRoutes(app);
+  console.log(
+    `App is running at http://localhost:${app.get('port')} in ${app.get('env')}`
+  );
+  console.log('Press CTRL-C to stop ');
+  displayRoutes(app);
 });
