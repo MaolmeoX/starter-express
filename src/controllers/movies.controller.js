9119d48f2d1997951a01b3de2a23eea79b787342
MaolmeoX <arnaud.maurice.pro@gmail.com>
2020-02-12 14:15:10 +0100

    feat: get all movies

--- src/controllers/movies.controller.js
+++ src/controllers/movies.controller.js
@@ -4,13 +4,14 @@ import MovieApiServices from '../services/MovieApi.services';
 export const getMovies = asyncHandler(async (req, res, next) => {
   const { query } = req.query;
 
-  res.status(201).json(
-    await MovieApiServices.get('search/movie', {
-      params: {
-        query,
-      },
-    })
-  );
+  const movies = await MovieApiServices.get('search/movie', {
+    params: {
+      query,
+      page: 1,
+    },
+  });
+
+  res.status(201).json(movies);
 });
 
 export const getMovie = asyncHandler(async (req, res, next) => {
--- src/services/MovieApi.services.js
+++ src/services/MovieApi.services.js
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import qs from 'qs';
+import async from 'async';
 
 class MovieApiServices {
   constructor() {
@@ -24,11 +25,44 @@ class MovieApiServices {
       config.params = newParams;
       return config;
     });
+
+    this.client.interceptors.request.use(request => {
+      const d = Date.now();
+      console.log(d, request.method, request.url, request.params);
+      return request;
+    });
   }
 
   async get(url, options) {
+    console.log('GET Options', options);
     const response = await this.client.get(url, options);
+    console.log('GET Options', options);
     return response.data;
+  }
+
+  async getAllPages(url, options) {
+    const result = await this.get(url, options);
+    // eslint-disable-next-line camelcase
+    const { total_pages, results } = result;
+
+    // eslint-disable-next-line camelcase
+    const resultPages = Array(total_pages - 1)
+      .fill()
+      .map((e, i) => i + 2);
+
+    const promiseResults = await async.mapSeries(resultPages, async index => {
+      const newOptions = { ...options };
+      newOptions.params.page = index;
+      const data = await this.get(url, newOptions);
+      return data.results;
+    });
+
+    const allResults = promiseResults.map(promiseResult => {
+      return promiseResult[0];
+    });
+
+    const final = [...results, ...allResults];
+    return final;
   }
 }
 
